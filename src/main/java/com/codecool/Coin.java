4bdf75727ebbbfeaf3e1e3e2e209e5ae0243a566
Jasio <mcyanek@gmail.com>
2020-10-05 13:21:23 +0200

    implements Coin enum

---
+++ src/main/java/com/codecool/Coin.java
@@ -0,0 +1,20 @@
+package com.codecool;
+
+public enum Coin {
+    FIVE(5f),
+    TWO(2f),
+    ONE(1f),
+    HALF(0.5f),
+    QUARTER(0.25f),
+    TENTH(0.1f);
+
+    private final float ammount;
+
+    private Coin(float ammount){
+        this.ammount = ammount;
+    }
+
+    public float getAmmount(){
+        return ammount;
+    }
+}
