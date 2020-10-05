40a63f327513e94c1d8b3fbbbe58696e933c53ba
Jasio <mcyanek@gmail.com>
2020-10-05 13:58:09 +0200

    introduces packages

--- src/main/java/com/codecool/Model/Coin.java
+++ src/main/java/com/codecool/Model/Coin.java
@@ -1,4 +1,4 @@
-package com.codecool;
+package com.codecool.Model;
 
 public enum Coin {
     FIVE(5f),
--- src/main/java/com/codecool/Storage/Storage.java
+++ src/main/java/com/codecool/Storage/Storage.java
@@ -1,4 +1,4 @@
-package com.codecool;
+package com.codecool.Storage;
 
 import java.util.HashMap;
 
