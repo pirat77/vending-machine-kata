dc5d92502d0bdff3e2accf5ef080036ade13cd2c
Jasio <mcyanek@gmail.com>
2020-10-05 21:19:39 +0200

    refines flowchart, covering edge cases

---
+++ src/test/java/com/codecool/Storage/CoinStorageTest.java
@@ -0,0 +1,30 @@
+package com.codecool.Storage;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CoinStorageTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void addElement() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void hasElement() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void clearBuffer() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void removeElements() {
+    }
+}
